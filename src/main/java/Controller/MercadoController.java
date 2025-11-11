d3f2a8cb257e762de6c34b1df0850352d5e0783a
Ana Luísa Coleone <isacoleone27@gmail.com>
2025-11-11 14:37:31 -0300

    Funcionarios adicionados

--- src/main/java/Controller/MercadoController.java
+++ src/main/java/Controller/MercadoController.java
@@ -2,25 +2,33 @@ package Controller;
 import Model.*;
 import View.*;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MercadoController {
     private List<Produto> catalogoProdutos;
     private Carrinho carrinhoAtual;
+    private List<Funcionario> funcionarios;
     private Main view;
  
 
     public MercadoController(Main view){
         this.view = view;
-            this.carrinhoAtual = new Carrinho();
-            this.catalogoProdutos = new ArrayList<>();
+        this.carrinhoAtual = new Carrinho();
+        this.catalogoProdutos = new ArrayList<>();
+        this.funcionarios = new ArrayList<>();
             
-            
-            catalogoProdutos.add(new Produto("Arroz ", "Tio Jõao", 10.0f, "Grãos", "0001"));
-            catalogoProdutos.add(new Produto("Feijão ", "Urbano", 4.99f, "Grãos", "6654"));
-            catalogoProdutos.add(new Produto("Leite Desnatado", "Itambé", 5.50f, "Laticineos", "56547"));
-            catalogoProdutos.add(new Produto("Pão de Forma", "Bauduco", 8.99f, "Pães", "0005"));
+        // produtos do mercado
+        catalogoProdutos.add(new Produto("Arroz", "Tio Jõao", 10.0f, "Grãos", "0001"));
+        catalogoProdutos.add(new Produto("Feijão", "Urbano", 4.99f, "Grãos", "6654"));
+        catalogoProdutos.add(new Produto("Leite Desnatado", "Itambé", 5.50f, "Laticineos", "56547"));
+        catalogoProdutos.add(new Produto("Pão de Forma", "Bauduco", 8.99f, "Pães", "0005"));
+
+        // funcionarios do mercado
+        funcionarios.add(new Gerente("Maria Silva", LocalDate.of(1985, 3, 12), "123.456.789-00", "Feminino"));
+        funcionarios.add(new Estocador("João Souza", LocalDate.of(1998, 10, 25), "987.654.321-00", "Masculino"));
+        funcionarios.add(new Estocador("Ana Lima", LocalDate.of(2000, 5, 7), "456.789.123-00", "Feminino"));
     }
     
     public void iniciar(){
@@ -55,6 +63,13 @@ public class MercadoController {
                     else {
                         view.exibirMensagem("Produto não encontrado! ");
                     }
+                    break;
+                case 4: // ver funcionarios
+                    List<String> listaFormatada = new ArrayList<>();
+                    for (Funcionario f : funcionarios) {
+                        listaFormatada.add(f.getClass().getSimpleName() + ": " + f.getNome() + " (" + f.getCPF() + ")");
+                    }
+                    view.exibirFuncionarios(listaFormatada);
                     break;
                 case 0: // sair
                     view.exibirMensagem("Saindo do sistema... ");
