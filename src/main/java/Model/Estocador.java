a101d94ddf853bfb1d42c29f12fc05e4006463f0
Ana Giulia Aguiar <166962330+imanagiulia@users.noreply.github.com>
2025-11-11 14:11:01 -0300

    Update Estocador.java

--- src/main/java/Model/Estocador.java
+++ src/main/java/Model/Estocador.java
@@ -2,6 +2,13 @@ package Model;
 
 import java.time.LocalDate;
 
+
+/*
+ * Principio High Cohesion 
+ * 
+ * Classe onde os atributos e os métodos são apenas sobre o funcionário estocador, não misturando com outros funcionários e nem fazendo tarefas que não pertence a ele.
+ */
+
 public class Estocador extends Funcionario{
 
     public Estocador(String nome, LocalDate data_nasc, String cpf, String sexo) {
